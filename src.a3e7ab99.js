22a17ed8456ab5995332d7f63d4d4c8a237d3405
riko1212 <46224717+riko1212@users.noreply.github.com>
2022-04-22 09:37:07 +0000

    Deploying to gh-pages from @ riko1212/bc-18-project@1b55693aa2ff6970b47065487d59663d4046a126 🚀

--- src.a3e7ab99.js
+++ src.a3e7ab99.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/bc-18-project/src.8c92eeb7.js.map
+//# sourceMappingURL=/bc-18-project/src.a3e7ab99.js.map
